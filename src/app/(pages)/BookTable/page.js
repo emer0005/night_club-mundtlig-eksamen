b9c3e36f5e1dc6437d569f5d8312df3ebb2a4243
Margrethe1316 <maso0012@stud.kea.dk>
2026-06-11 09:38:46 +0200

    Update Guest component to utilize reservations data; enhance guest selection logic and table availability checks

--- src/app/(pages)/BookTable/page.js
+++ src/app/(pages)/BookTable/page.js
@@ -2,12 +2,15 @@ import Header from "@/components/GlobalComponents/Header";
 import Guest from "@/components/BookTable/Guest";
 
 export default async function BookTablePage() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/events`);
-  const events = await response.json();
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/reservations`,
+  );
+  const reservations = await response.json();
+
   return (
     <>
       <Header />
-      <Guest />
+      <Guest reservations={reservations} />
     </>
   );
 }
--- src/components/BookTable/Guest.jsx
+++ src/components/BookTable/Guest.jsx
@@ -3,8 +3,9 @@
 import Image from "next/image";
 import FormBar from "./FormBar";
 import { useRouter, useSearchParams } from "next/navigation";
+import {tables} from "@/app/data/tables";
 
-const Guest = () => {
+const Guest = ({ reservations }) => {
   const router = useRouter();
   const params = useSearchParams();
   const eventId = params.get("eventId");
@@ -16,6 +17,22 @@ const Guest = () => {
       router.push(`/BookTable/SelectEvent?guests=${guests}`);
     }
   }
+
+
+    const reservation = reservations.filter((res) => res.eventId === Number(eventId));  
+    const tableID = reservation.map((res) => Number(res.table));
+    const availableTables = tables.filter((table) => !tableID.includes(table.id));
+  
+    function IsGuestAllowed(guests){
+      const isAllowed = availableTables.some((table) => table.guest >= guests);
+      return isAllowed;
+    }
+
+    console.log(reservation);
+    console.log(eventId);
+    console.log(tableID);
+    console.log(availableTables);
+    console.log(IsGuestAllowed(4));
 
   return (
     <div className="p-7 md:p-0 grid grid-cols-1 lg:grid-cols-2 gap-15 min-h-[calc(100vh-80px)] place-content-center items-start mx-auto max-w-[1200px]">
@@ -37,10 +54,12 @@ const Guest = () => {
           {[1, 2, 3, 4, 5, 6, 7, 8].map((number) => (
             <button
               key={number}
+              disabled={!IsGuestAllowed(number)}
               onClick={() => handleGuestSelection(number)}
-              className="bg-black border border-[var(--color-surface-highlight-primary)]
-                         text-white w-[70px] h-[70px] text-xl font-semibold cursor-pointer 
-                         hover:bg-[color-mix(in_srgb,var(--color-surface-highlight-primary)_20%,transparent)] transition-all duration-200"
+              className={`bg-black border border-[var(--color-surface-highlight-primary)]
+                         text-white w-[70px] h-[70px] text-xl font-semibold 
+                
+                         ${!IsGuestAllowed(number) ? "opacity-50 cursor-not-allowed" : "cursor-pointer hover:bg-[color-mix(in_srgb,var(--color-surface-highlight-primary)_20%,transparent)] transition-all duration-200"}`}
             >
               {number}
             </button>
