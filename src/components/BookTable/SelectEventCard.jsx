23915e13b4689d6b6f2c38bec949bf69623a19ab
Emilie <emer0005@stud.kea.dk>
2026-06-15 11:37:41 +0200

    Update opacity for unavailable tables in SelectEventCard component for improved visibility

--- src/components/BookTable/SelectEventCard.jsx
+++ src/components/BookTable/SelectEventCard.jsx
@@ -33,7 +33,7 @@ const SelectEventCard = ({ name, date, location, img, id, reservations }) => {
     <div className={`relative flex flex-row items-center gap-4 border mb-4 p-2 transition-all duration-200 border-[var(--color-surface-highlight-primary)] ${Number(event) === id ? "bg-[color-mix(in_srgb,var(--color-surface-highlight-primary)_20%,transparent)]" : ""} ${availableTables.length === 0 ? "" : "hover:bg-[color-mix(in_srgb,var(--color-surface-highlight-primary)_20%,transparent)] cursor-pointer"} `} onClick={availableTables.length === 0 ? undefined : () => handleEventSelection(id)}>
       {availableTables.length === 0 && <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-[1.2rem] uppercase font-bold">No Tables Available </span>}
       <Image src={`${process.env.NEXT_PUBLIC_API_URL}${img}`} alt="Guest" width={100} height={100} className={`${availableTables.length === 0 ? "opacity-10" : ""}`} />
-      <div className={`${availableTables.length === 0 ? "opacity-10" : ""}`}>
+      <div className={`${availableTables.length === 0 ? "opacity-30" : ""}`}>
         <h6 className="text-white text-xl font-semibold uppercase">{name}</h6>
         <span className="text-white">{eventDate(date)} </span>
         <span className="text-[var(--color-text-body)]"> | </span>
